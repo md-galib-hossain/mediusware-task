6589a60465ba99490d7831a15d0c2694ba21213d
md-galib-hossain <mdgalib23@gmail.com>
2025-04-17 12:53:28 +0600

    feat: blog card static with initial design

--- src/components/Ui/BlogCard/BlogCard.tsx
+++ src/components/Ui/BlogCard/BlogCard.tsx
@@ -3,12 +3,26 @@ import React from 'react'
 const BlogCard = ({blogCard}) => {
   return (
     <div className=' rounded-2xl'>
-       <div className='max-w-[544px] h-[300px]'>
+       <div className='max-w-[544px]'>
        <img src={"/blog.png"} alt="Blog Card" className='w-full h-full'/>
 
        </div>
-       <div className='bg-white h-10 p-4'>
+       <div className='bg-white p-4'>
 
+<div className='flex items-center gap-4'>
+<img className='w-10 h-10 rounded-full' src={"/avatar.png"} alt="User" />
+<div className='text-sm'>
+<p>Ronald Richards</p>
+<p className='text-[#667085]'>20 Jan 2022</p>
+
+</div>
+
+
+    </div>
+
+<p className='text-2xl font-medium text-[#002B4F]'>
+UX Lessons That We Can Learn from “Threads”
+</p>
        </div>
     </div>
   )
